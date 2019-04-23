1ba0fb740e5a15aa035836bc755b37446f4db683
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2019-04-23 10:24:11 +0100

    add a helper function that find format of the date object

---
+++ getDateFormat.php
@@ -0,0 +1,67 @@
+<?php 
+
+/**
+ * Get date format from date object.
+ * 
+ * @param $date
+ * @return string
+ */
+function getDateFormat($date)
+{
+	$postfix = null;
+	$prefix;
+	$separators = ['/', '-', '.'];
+	$separator;
+	$dayindex;
+	$monthindex;
+	$yearindex;
+
+	if(strpos($date, ' ')) {
+		$d = explode(' ', $date);
+		$prefix = current($d);
+		$postfix = end($d);
+	} else {
+		$prefix = $date;
+	}
+
+
+	foreach ($separators as $key) {
+		if (strpos($date, $key)) {
+			$separator = $key;
+		}
+	}
+
+	$data = explode($separator, $prefix);
+
+	if (count($data) == 3) {
+		$dayindex = strlen(end($data)) == 4 ? 0 : 2;
+		foreach ($data as $index => $value) {
+			if(strlen($value) == 4) {
+				$yearindex = $index;
+			}
+			if ($value >= 1 && $value <=12) {
+				$monthindex = $dayindex == 2 ? 1 : $index;
+			}
+			if ($value >= 13 && $value <=31) {
+				$dayindex = $index;
+			}
+		}
+	}
+
+	$data[$dayindex] = 'd';
+	$data[$monthindex] = 'm';
+	$data[$yearindex] = 'Y';
+
+	$data = implode($separator, $data);
+
+	if (!is_null($postfix)) {
+		$content = explode(':', $postfix);
+		if (count($content) == 2) {
+			return $data . ' H:i';
+		} else {
+			return $data . ' H:i:s';
+		}
+	}
+
+	return $data;
+}
